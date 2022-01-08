e3aa8efa211456bf5a77e6acbc288184523324a0
Thomas Chaffey <tlc37@cam.ac.uk>
2022-01-08 16:24:00 +0000

    setpoint can be set with rotary encoder

--- main/main.ino
+++ main/main.ino
@@ -1,16 +1,28 @@
 
+// At the moment, this is a rapid prototype written without any object orientedness.
+// Final version will become OO.
 
 #include <arduino-timer.h>
-# define SW 6
+#include <RotaryEncoder.h>
 
+// terminal definitions
+#define SW 6
+#define DT 5
+#define CLK 4
+
+// globals for setpoint
+float r = 92.0;
+RotaryEncoder encoder(CLK, DT, RotaryEncoder::LatchMode::TWO03);
+
+
+// globals for the stopwatch
 unsigned long lastButtonPress = 0;
 bool stopwatchOn = false;
-
 auto timer = timer_create_default(); // create a timer with default settings
 Timer<>::Task stopwatch;
+float t = 0.0; // stopwatch time
 
-float t;
-
+// stopwatch function
 bool increment_time(void *) {
   t += 0.5;
   Serial.println(t);
@@ -18,13 +30,27 @@ bool increment_time(void *) {
 }
 
 void setup() {
-  t = 0.0; // start stopwatch from 0
   Serial.begin(9600);
   pinMode(SW, INPUT_PULLUP);
 
 }
 
 void loop() {
+
+  // setpoint section
+  encoder.tick();
+
+  static int pos = 0;
+  int newPos = encoder.getPosition();
+  if (pos != newPos) {
+    r += 0.5 * (float)encoder.getDirection();
+    Serial.println(r);
+    pos = newPos;
+  }
+
+  
+
+  // stopwatch section
   timer.tick(); // tick the timer
 
    // Read the button state
