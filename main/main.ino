f3ec444776b2ccb3b113b9ed7e153a67c061e5d9
Thomas Chaffey <tlc37@cam.ac.uk>
2022-01-08 19:03:59 +0000

    Display code with u8g2, way too slow, breaks everything

--- main/main.ino
+++ main/main.ino
@@ -4,16 +4,24 @@
 
 #include <arduino-timer.h>
 #include <RotaryEncoder.h>
+#include <U8g2lib.h>
+#include <Wire.h>
 
 // terminal definitions
 #define SW 6
 #define DT 5
 #define CLK 4
+#define PWM 9
+
+// screen init
+//U8G2_SH1106_128X64_NONAME_F_HW_I2C u8g2(U8G2_R0, /* reset=*/ U8X8_PIN_NONE);
+U8GLIB_SH1106_128X64 u8g(U8G_I2C_OPT_NO_ACK);  // Display which does not send ACK
 
 // globals for setpoint
+#define REFERENCE 100
+#define PWM_PERIOD 500
 float r = 92.0;
 RotaryEncoder encoder(CLK, DT, RotaryEncoder::LatchMode::TWO03);
-
 
 // globals for the stopwatch
 unsigned long lastButtonPress = 0;
@@ -22,6 +30,9 @@ auto timer = timer_create_default(); // create a timer with default settings
 Timer<>::Task stopwatch;
 float t = 0.0; // stopwatch time
 
+// globals for PWM
+unsigned long lastPWMCycle = 0;
+
 // stopwatch function
 bool increment_time(void *) {
   t += 0.5;
@@ -29,10 +40,28 @@ bool increment_time(void *) {
   return true;
 }
 
+// timer function for turning heat off
+bool turn_heat_off(void *) {
+  digitalWrite(PWM, LOW);
+  return true;
+}
+
+// status printing function
+bool print_status(void *) {
+  u8g2.clearBuffer();
+  u8g2.setCursor(0, 16);
+  u8g2.print(r);
+  u8g2.sendBuffer();
+  return true;
+}
+  
+
 void setup() {
   Serial.begin(9600);
   pinMode(SW, INPUT_PULLUP);
-
+  u8g2.begin();
+  u8g2.setFont(u8g2_font_logisoso16_tf);
+  Wire.setClock(400000); 
 }
 
 void loop() {
@@ -44,14 +73,24 @@ void loop() {
   int newPos = encoder.getPosition();
   if (pos != newPos) {
     r += 0.5 * (float)encoder.getDirection();
+    if(r < 0) r = 0;
+    if(r > REFERENCE) r = REFERENCE;
     Serial.println(r);
     pos = newPos;
   }
 
-  
-
   // stopwatch section
   timer.tick(); // tick the timer
+
+  // heat control
+  unsigned long thisPWMCycle = millis();
+  if (thisPWMCycle - lastPWMCycle >= PWM_PERIOD) {
+    float error = REFERENCE - r;
+    int upTime = error > 0 ? floor((REFERENCE - r)*PWM_PERIOD/100) : 0; //time in ms
+    digitalWrite(PWM, HIGH);
+    if (upTime < PWM_PERIOD) timer.in(upTime, turn_heat_off);
+    lastPWMCycle = thisPWMCycle;
+  }
 
    // Read the button state
   int btnState = digitalRead(SW);
@@ -65,10 +104,10 @@ void loop() {
       {
         timer.cancel(stopwatch);
         stopwatchOn = false;
-        t = 0.0;
       } else
       {
         // start the stopwatch, increment every half second
+        t = 0.0;
         stopwatch = timer.every(500, increment_time);
         stopwatchOn = true;
       }
@@ -78,6 +117,7 @@ void loop() {
     lastButtonPress = millis();
   }
 
-  
+    // update screen every 0.2s
+    timer.every(1000, print_status);
 
 }
